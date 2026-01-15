b86ca41618429ca8ee836bef3164d87bd321b5dc
sujeong.lee <developercosj@gmail.com>
2026-01-15 16:17:00 +0900

    Feat: 첫 프로젝트 세팅

---
+++ src/main/kotlin/com/soul/soulsonder/SoulSonderApplication.kt
@@ -0,0 +1,11 @@
+package com.soul.soulsonder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoulSonderApplication
+
+fun main(args: Array<String>) {
+    runApplication<SoulSonderApplication>(*args)
+}
---
+++ src/test/kotlin/com/soul/soulsonder/SoulinspirationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.soul.soulsonder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SoulinspirationApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
